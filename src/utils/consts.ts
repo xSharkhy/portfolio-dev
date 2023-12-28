9b223bd3aa7221c0c1f285cfdc77775a948bddeb
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2023-12-29 00:37:22 +0100

    fix: correct lorem ipsum text on consts

--- src/utils/consts.ts
+++ src/utils/consts.ts
@@ -3,7 +3,7 @@ export const EXPERIENCES = [
     date: "Marzo 2022",
     title: "Desarrollador Web - La Mina Estudio",
     description:
-      "Desarrollo de herramientas de scraping y crawlers para la obtención de datos de sitios web.",
+      "Desarrollo de herramientas de scraping y crawlers para la obtención de datos de sitios web. Optimización de procesos de obtención de datos y automatización de tareas. Implementación de Bases de Datos con grandes cantidades de entradas, SQL y NoSQL.",
   },
 ]
 
@@ -12,12 +12,12 @@ export const EDUCATION = [
     date: "Actualmente...",
     title: "Máster en Creación de Videojuegos y Realidad Virtual - IES Abastos",
     description:
-      "Cursando la carrera de Ingeniería en Sistemas de Información en la UTN.",
+      "Cursando Desarrollo de Videojuegos para profundizar conocimientos en el desarrollo de software  y esperiencias interactivas.",
   },
   {
     date: "2021 - 2023",
     title: "Técnico Superior en Desarrollo de Aplicaciones Web - IES Serpis",
     description:
-      "Técnico en Programación de Sistemas Informáticos en la EEST N° 1.",
+      "",
   },
 ]
