e3dc497c7aa0d4c356127fbd449f6190460229f7
Ismael Morejón Blasco <93212283+xSharkhy@users.noreply.github.com>
2023-12-29 00:25:20 +0100

    chore: add constants to represent timelines

---
+++ src/utils/consts.ts
@@ -0,0 +1,23 @@
+export const EXPERIENCES = [
+  {
+    date: "Marzo 2022",
+    title: "Desarrollador Web - La Mina Estudio",
+    description:
+      "Desarrollo de herramientas de scraping y crawlers para la obtención de datos de sitios web.",
+  },
+]
+
+export const EDUCATION = [
+  {
+    date: "Actualmente...",
+    title: "Máster en Creación de Videojuegos y Realidad Virtual - IES Abastos",
+    description:
+      "Cursando la carrera de Ingeniería en Sistemas de Información en la UTN.",
+  },
+  {
+    date: "2021 - 2023",
+    title: "Técnico Superior en Desarrollo de Aplicaciones Web - IES Serpis",
+    description:
+      "Técnico en Programación de Sistemas Informáticos en la EEST N° 1.",
+  },
+]
